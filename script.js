55296bb20a3d225fe703cc58c34a90a4510359e4
mjdev <jervinvillones6@gmail.com>
2026-01-01 21:07:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//const menu = document.getElementById("menutoggle");
+//const nav = document.querySelector(".navlinks")
+//const Close = document.querySelector(".close")
+
+//menu.addEventListener("click", () =>{
+ //   nav.classList.toggle("open");
+//});
+
+//Close.addEventListener("click", () =>{
+ //   nav.classList.toggle("close");
+//});
